11c4e9f09a813c6dc7c1c7159cce8f0cb8425c3f
unknown <2329519993@qq.com>
2020-03-13 16:30:05 +0800

    完成商品功能的开发

--- src/main.js
+++ src/main.js
@@ -5,7 +5,13 @@ import router from './router'
 import store from './store'
 import './plugins/element.js'
 import axios from 'axios'
+// 树型表格组件
 import TreeTable from 'vue-table-with-tree-grid'
+// 富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 // 字体图标
 import './assets/fonts/iconfont.css'
 // 全局样式
@@ -20,6 +26,19 @@ axios.interceptors.request.use(config => {
 })
 Vue.config.productionTip = false
 Vue.component('tree-table', TreeTable)
+
+Vue.use(VueQuillEditor)
+
+Vue.filter('dateFormat', originVal => {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const h = (dt.getHours() + '').padStart(2, 0)
+  const minu = (dt.getMinutes() + '').padStart(2, '0')
+  const s = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${h}:${minu}.${s}`
+})
 new Vue({
   router,
   store,
